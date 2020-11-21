b6e7850e79fa1e5dd1a3e23d644f5792a4210d01
Ian-c-cameron <ian.c.cam@gmail.com>
2020-11-20 18:55:18 -0800

    calendar test demo

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -2,7 +2,57 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 import 'fontsource-roboto';
+// import { Calendar, momentLocalizer } from 'react-big-calendar';
+// import 'react-big-calendar/lib/css/react-big-calendar.css';
 
+// import moment from 'moment'
+// import Paper from '@material-ui/core/Paper';
+
+// Setup the localizer by providing the moment (or globalize) Object
+// to the correct localizer.
+const localizer = momentLocalizer(moment) // or globalizeLocalizer
+
+// const events = [{
+//   id: 0,
+//   title: 'All Day Event very long title',
+//   allDay: true,
+//   start: new Date(2020, 11, 0),
+//   end: new Date(2020, 11, 1),
+// },
+// {
+//   id: 1,
+//   title: 'Long Event',
+//   start: new Date(2020, 11, 7),
+//   end: new Date(2020, 11, 10),
+// },
+
+// {
+//   id: 2,
+//   title: 'DTS STARTS',
+//   start: new Date(2020, 11, 13, 0, 0, 0),
+//   end: new Date(2020, 11, 20, 0, 0, 0),
+// },
+
+// {
+//   id: 3,
+//   title: 'DTS ENDS',
+//   start: new Date(2020, 11, 6, 0, 0, 0),
+//   end: new Date(2020, 11, 13, 0, 0, 0),
+// },
+
+// {
+//   id: 4,
+//   title: 'Some Event',
+//   start: new Date(2020, 11, 9, 0, 0, 0),
+//   end: new Date(2020, 11, 10, 0, 0, 0),
+// },
+// {
+//   id: 5,
+//   title: 'Conference',
+//   start: new Date(2020, 11, 11),
+//   end: new Date(2020, 11, 13),
+//   desc: 'Big conference for important people',
+// }];
 class App extends Component {
   constructor(props) {
     super(props)
@@ -24,13 +74,25 @@ class App extends Component {
     }) 
   }
 
+  
+
   render() {
     return (
       <div className="App">
         <h1>{ this.state.message }</h1>
         <button onClick={this.fetchData} >
           Fetch Data
-        </button>        
+        </button>
+        <div className="cal_container">
+          {/* <Calendar
+            events={events}
+            startAccessor="start"
+            endAccessor="end"
+            defaultDate={moment().toDate()}
+            localizer={localizer}
+          />
+          <Paper elevation={3} /> */}
+        </div>
       </div>
     );
   }
