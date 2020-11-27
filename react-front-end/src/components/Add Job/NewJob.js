b8110e85d95d55bedb55e2365de91a8419190c93
Mohamedsa1990 <Mohameda1990@gamil.com>
2020-11-26 17:28:07 -0700

    fix the library bug

--- react-front-end/src/components/Add Job/NewJob.js
+++ react-front-end/src/components/Add Job/NewJob.js
@@ -3,7 +3,7 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import { FormControl, Select, InputLabel, MenuItem} from '@material-ui/core'
-import { TimePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
+
 
 export default function NewJob({travelTime, setTravelTime, status, setStatus, jobname, setJobName, firstName, setFirstName, lastName, setLastName, address,setAddress
   , city, setCity,phoneNumber, setPhoneNumber, email, setEmail, notes, setNotes,errorState }) {
