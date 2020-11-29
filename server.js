83aaaa3d6ec2131d9793f6ba56cd4583054bec1b
Al Lotina <lotina.al@gmail.com>
2020-11-28 20:43:12 -0800

    master setup for deployment

--- server.js
+++ server.js
@@ -46,6 +46,8 @@ App.get('/api/data', (req, res) => res.json({
   message: "Seems to work!",
 }));
 
+App.use(Express.static('./react-front-end/build'));
+
 App.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);
