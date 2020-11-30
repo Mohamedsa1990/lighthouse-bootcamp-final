34c72fbfb08e754f59785aad6ec6206080a402dd
Mohamedsa1990 <Mohameda1990@gamil.com>
2020-11-30 16:14:08 -0700

    add transition and style the header

--- react-front-end/src/components/Cover.js
+++ react-front-end/src/components/Cover.js
@@ -1,19 +1,28 @@
 import React from 'react';
-import Box from '@material-ui/core/Box';
-import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button'
 import './Cover.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
 
 export default function LandingPage(){
 
   return (
-    <div className={'Cover'}>
-      <Link to="/app">
+    <motion.div 
+    className={'Cover'}
+    initial={{ scale: 0 }}
+    animate={{ rotate: 360, scale: 1 }}
+    transition={{
+    type: "spring",
+    stiffness: 260,
+    damping: 20
+    }}
+    >
+      <NavLink to="/app">
         <Button variant="outlined" size={"large"} style={{color: "White", borderColor: "White", marginTop: "500px"}}>
           Get started
         </Button>
-      </Link>
-    </div>
+      </NavLink>
+    </motion.div>
   );
 }
